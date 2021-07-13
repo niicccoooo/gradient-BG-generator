ff4cdb7df5299a9c97f216a929410b52aac7822c
Nicolas Gomez <77043795+niicccoooo@users.noreply.github.com>
2021-07-13 02:52:40 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let css = document.querySelector("h3");
+let color1 = document.querySelector(".color1");
+let color2 = document.querySelector(".color2");
+let body = document.getElementById("gradient");
+let randomBttn = document.querySelector("button");
+
+
+function randomBackground() {
+	color1.value = "#" + Math.floor(Math.random()*16777215).toString(16);
+	color2.value = "#" + Math.floor(Math.random()*16777215).toString(16);
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+
+}
+	
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}		
+
+
+randomBttn.onclick = randomBackground;
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
